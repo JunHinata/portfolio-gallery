78258c27d4287dec773f459c77054e5769616f51
JunHinata <67004258+JunHinata@users.noreply.github.com>
2021-12-05 21:47:50 +0900

    first commit
    ポートフォリオ-ギャラリーサイト

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/* Ligthbox Luminous */
+new LuminousGallery(document.querySelectorAll('.grid-gallery'), {}, {
+  caption: function(trigger) {
+    return trigger.querySelector('img').getAttribute('alt');
+  }
+});
+
+/* Fadeup Animation AOS */
+AOS.init({
+  duration: 1000
+});
